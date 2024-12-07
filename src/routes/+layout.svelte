cd23e408a1b877e8b1928103dc2bf4ee4ef2dfbf
John Larson <john@jjlarson.com>
2024-12-06 19:15:40 -0600

    updated padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 	let { children } = $props();
 </script>
 
-<main class="flex min-h-screen w-screen flex-col items-center justify-between">
+<main class="flex min-h-screen w-screen flex-col items-center justify-between py-6">
 	{@render children()}
 </main>
